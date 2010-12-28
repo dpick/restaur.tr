8f79df0e8c7405ad70213e0a843bc9fb592b1e49
Derek Hammer <derek.r.hammer@gmail.com>
2010-12-28 16:26:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestaurTr::Application.config.secret_token = '6a0f93e430b4d025827918a05ac6e5fc746feb0767645f37fea7ea7f59f5a5221d173330496c2ebe0a1472af7dc4e7666b48c4842113ad988d5ae3b89871c5af'
